92d30cdc66ae368d3069d31e404c0e49ef85ccc4
Alex Rezit <AlexRezit@gmail.com>
2013-02-03 11:47:21 +0800

    Add RSocialShare.

---
+++ RSocialDemo/RSocialShare.h
@@ -0,0 +1,71 @@
+//
+//  RSocialShare.h
+//  RSocialDemo
+//
+//  Created by Alex Rezit on 29/01/2013.
+//  Copyright (c) 2013 Seymour Dev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, RSocialShareType) {
+    RSocialShareTypeDefault,
+    RSocialShareTypeText,
+    RSocialShareTypeImage,
+    RSocialShareTypeLink,
+    RSocialShareTypeMultiType = RSocialShareTypeDefault
+};
+
+typedef NS_ENUM(NSInteger, RSocialShareLinkType) {
+    RSocialShareLinkTypeDefault,
+    RSocialShareLinkTypeImage,
+    RSocialShareLinkTypeAudio,
+    RSocialShareLinkTypeVideo,
+};
+
+@class RSocialShare;
+
+@protocol RSocialShareDelegate <NSObject>
+
+@optional
+- (void)socialShareDidCancel:(RSocialShare *)socialShare;
+- (void)socialShare:(RSocialShare *)socialShare didFinishWithStatus:(NSDictionary *)status;
+- (void)socialShare:(RSocialShare *)socialShare didFailWithError:(NSError *)error;
+
+@end
+
+@interface RSocialShare : NSObject
+
+@property (nonatomic, assign) id<RSocialShareDelegate> delegate;
+
+@property (nonatomic, readonly) RSocialOAuth *auth;
+
+// Config
+@property (nonatomic, assign) RSocialShareType type;
+
+// Text
+@property (nonatomic, strong) NSString *content;
+
+// Image
+@property (nonatomic, strong) UIImage *image; // Upload
+@property (nonatomic, strong) NSString *imageLink; // Use web image
+
+// Link (including audio and video)
+@property (nonatomic, strong) NSString *link;
+@property (nonatomic, assign) RSocialShareLinkType linkType;
+@property (nonatomic, strong) NSString *linkTitle;
+@property (nonatomic, strong) NSString *linkDescription;
+@property (nonatomic, strong) NSString *linkImageLink;
+
+#pragma mark - Getters and setters
+// Implement this method to return a shared instance of RSocialOAuth.
+- (RSocialOAuth *)auth;
+
+#pragma mark - Actions
+- (void)showForm;
+
+#pragma mark - Share flow
+// Implement this method to send share request.
+- (void)sendFormWithCompletionHandler:(void (^)(BOOL success, NSDictionary *status, NSError *error))completion;
+
+@end
---
+++ RSocialDemo/RSocialShare.m
@@ -0,0 +1,59 @@
+//
+//  RSocialShare.m
+//  RSocialDemo
+//
+//  Created by Alex Rezit on 29/01/2013.
+//  Copyright (c) 2013 Seymour Dev. All rights reserved.
+//
+
+#import "RSocialOAuth.h"
+#import "RSocialShare.h"
+
+@interface RSocialShare ()
+
+- (void)share;
+
+@end
+
+@implementation RSocialShare
+
+#pragma mark - External
+
+- (void)showForm
+{
+#warning Show share form.
+}
+
+#pragma mark - Share flow
+
+- (void)share
+{
+    [self.auth authorizeWithCompletionHandler:^(BOOL success) {
+        if (success) {
+            [self sendFormWithCompletionHandler:^(BOOL success, NSDictionary *status, NSError *error) {
+                if (success) {
+                    if ([self.delegate respondsToSelector:@selector(socialShare:didFinishWithStatus:)]) {
+                        [self.delegate socialShare:self didFinishWithStatus:status];
+                    }
+                } else {
+                    if ([self.delegate respondsToSelector:@selector(socialShare:didFailWithError:)]) {
+                        [self.delegate socialShare:self didFailWithError:error];
+                    }
+                }
+            }];
+        } else {
+            if ([self.delegate respondsToSelector:@selector(socialShare:didFailWithError:)]) {
+                NSError *error = nil;
+#warning Add error.
+                [self.delegate socialShare:self didFailWithError:error];
+            }
+        }
+    }];
+}
+
+- (void)sendFormWithCompletionHandler:(void (^)(BOOL success, NSDictionary *status, NSError *error))completion
+{
+    NSAssert(0, @"RSocialShare: Method not implemented.");
+}
+
+@end
